975f5e771d493463de66466fe0a969626351cec6
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-04-04 17:13:36 +0900

    新規追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "boards#index"
+  get 'boards', to: "boards#index"
+  get 'boards/new', to: "boards#new"
+  post 'boards', to: "boards#create"
 end
