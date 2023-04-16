9449cc84e13014fbb90f04fbd7622b230112e217
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-04-16 22:19:53 +0900

    コメント作成画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   resources :boards
+  resources :comments, only: %i[create destroy]
 end
