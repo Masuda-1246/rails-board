f4ed01d660c8478fb970cd56eb7ecfd633a3a911
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-05-01 23:30:46 +0900

    ユーザー登録フォーム

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,19 @@
 #  index_users_on_name  (name) UNIQUE
 #
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :boards, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: {maximum:16},
+    format: {
+      with: /\A[a-z0-9]+\z/,
+      message: "は半角英数字で入力してください"
+    }
+  validates :password,
+    length: {minimum:8}
 end
