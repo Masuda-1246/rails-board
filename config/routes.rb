8535ef4778e13f352db36f6c30d3569c8ab9dae1
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-05-01 22:57:40 +0900

    ヘッダーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root 'boards#index'
   resources :boards
   resources :comments, only: %i[create destroy]
 end
