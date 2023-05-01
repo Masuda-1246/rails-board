ec0a85802a69f2e67d3861e999a8cbee51aad764
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-05-01 23:09:26 +0900

    ログイン周りのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/me'
+  get 'mypage', to: 'users#me'
   get 'home/index'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'boards#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
